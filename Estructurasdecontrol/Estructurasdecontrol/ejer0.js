83f0e89fc188381e889f54145f6de1bdfce40157
Janier Julian Cerón Coral <janierjceron2044@gmail.com>
2025-05-09 11:41:23 -0500

    Add files via upload

---
+++ Estructurasdecontrol/Estructurasdecontrol/ejer0.js
@@ -0,0 +1,60 @@
+if (5>7){
+    console.log("verdadero")
+}else{
+    console.log("Falso")
+}
+
+let dato="cine"
+let estadotiempo="verano"
+
+if (estadotiempo==="verano"){
+    console.log("va a " + dato)
+}else{
+    console.log("me quedo en casa")
+}
+
+let x=10
+
+if(x>5){
+    console.log("es mayor a 5")
+}
+
+{
+    let z=20
+}
+
+let valor=5
+
+if (valor===1){
+    console.log("Uno")
+}
+if (valor===1){
+    console.log("Dos")
+}
+if (valor===1){
+    console.log("Tres")
+}
+if (valor===1){
+    console.log("Cuatro")
+}
+if (valor===1){
+    console.log("Cinco")
+}
+
+if (valor!==1 && valor!==2 && valor!==3 && valor!==4 && valor!==5){
+    console.log("Diferente valor de 1 a 5")
+}
+
+if (valor===1){
+    console.log("Uno")
+}else if(valor===2){
+    console.log("Dos")
+}else if(valor===2){
+    console.log("Tres")
+}else if(valor===2){
+    console.log("Cuatro")
+}else if(valor===2){
+    console.log("Cinco")
+}else{
+    console.log("Otro valor")
+}
